b59951dc00282d0ca6e7c92ac5376beace45b506
sheikh-momin <momin.emon14@gmail.com>
2023-02-20 22:47:10 -0800

    pulling

--- index.js
+++ index.js
@@ -328,6 +328,12 @@ async function run() {
       const result = await semesterDrop.find(query).toArray();
       res.send(result);
     });
+    app.delete("/drop/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await semesterDrop.deleteOne(query);
+      res.send(result);
+    });
 
     // Notice
     app.post("/notice", async (req, res) => {
